d3bd7cfb1c5bae04a5b3f6eb1be2f600eadc0c2e
aleixcastells <castells.94@gmail.com>
2023-02-08 03:34:50 +0100

    Multi location and info formatting functions
    - Added a dropdown to select different locations
    - Added a better way to use the API URL with many locations
    - Added functionality that makes the navbar title change with different locations
    - Added a function that resets all the fields to a prefetch like state
    - Fixed an issue where night would not be detected
    - Changed the format of the date and current time
    - Changed the rain units from mm to L/m2
    
    And minor changes and fixes here and there.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
-const API_URL = ['Artà', 'https://api.open-meteo.com/v1/forecast?latitude=39.69&longitude=3.35&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation,rain,showers,freezinglevel_height,weathercode,surface_pressure,visibility,evapotranspiration,cape,windgusts_10m,soil_temperature_0cm,soil_temperature_6cm,soil_moisture_1_3cm,soil_moisture_3_9cm,soil_moisture_9_27cm,soil_moisture_27_81cm,shortwave_radiation_instant,direct_radiation_instant,diffuse_radiation_instant,direct_normal_irradiance_instant,terrestrial_radiation_instant&models=best_match&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,showers_sum,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&timezone=Europe%2FBerlin&past_days=2']
-
+const API_URL = [
+    ['https://api.open-meteo.com/v1/forecast?latitude=', '&longitude=', '&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation,rain,showers,freezinglevel_height,weathercode,surface_pressure,visibility,evapotranspiration,cape,windgusts_10m,soil_temperature_0cm,soil_temperature_6cm,soil_moisture_1_3cm,soil_moisture_3_9cm,soil_moisture_9_27cm,soil_moisture_27_81cm,shortwave_radiation_instant,direct_radiation_instant,diffuse_radiation_instant,direct_normal_irradiance_instant,terrestrial_radiation_instant&models=best_match&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,showers_sum,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&timezone=Europe%2FBerlin&past_days=2'],
+    ['1 Artà', '39.69', '3.35'],
+    ['2 Tokio', '35.69', '139.69'],
+    ['3 Barcelona', '41.39', '2.16'],
+    ['4 Sant Quirze', '41.53', '2.08'],
+    ['5 Gavà', '41.31', '2.00'],
+    ['6 Palma', '39.57', '2.65'],
+    ['7 Sydney', '-33.87', '151.21'],
+    ['8 Anchorage', '61.22', '-149.90'],
+    ['9 Winnipeg', '49.88', '-97.15'],
+    ['10 Salvador', '-12.97', '-38.51'],
+    ['11 Llucmajor', '39.49', '2.89'],
+    ['12 Berlin', '52.52', '13.41'],
+    ['13 Madrid', '40.42', '-3.70'],
+    ['14 Amsterdam', '52.37', '4.89'],
+    ['15 Estocolm', '59.33', '18.07'],
+    ['16 London', '51.51', '-0.13'],
+    ['17 Glasgow', '55.87', '-4.26'],
+    ['18 New York', '40.71', '-74.01'],
+    ['19 Lyon', '45.75', '4.85']
+]
 // API Documentation https://open-meteo.com/en/docs#api-documentation
+
+var location_number = 1
 
 const DATE = new Date()
 const INFO = {
     get hores() { return DATE.getHours() },
     get minuts() { return DATE.getMinutes() }
 }
+
+
 const WEATHER = {}
 const DATA = {}
 const PREVIOUS_NIGHT = {}
@@ -25,15 +49,15 @@ const INFO_GET = {
 const WEATHER_GET = {
     get hora() { return `${INFO.date_hora}` },
     get darrera_actualització() { return `${WEATHER.api_current_time.match(/.....$/)} h` },
-    get data_avui() { return `${INFO.api_current_date.match(/^........../)}` },
+    get data_avui() { return `${dateFormat()}` },
     get temperatura() { return `${WEATHER.api_temperature} °C` },
     get temperatura_aparent() { return `${WEATHER.api_apparent_temperature} °C` },
     get temps() { return weatherCode(WEATHER.api_weathercode) },
     get direcció_vent() { return `${windCardinalDirection(WEATHER.api_winddirection)}` },
     get vent() { return `${WEATHER.api_windspeed} Km/h` },
     get ratxes_vent() { return `${WEATHER.api_windgusts_10m} Km/h` },
-    get pluja_aquesta_hora() { return `${WEATHER.api_rain} L` },
-    get pluja_total_avui() { return `${WEATHER.api_precipitation_sum} L` },
+    get pluja_aquesta_hora() { return `${WEATHER.api_rain} L/m²` },
+    get pluja_total_avui() { return `${WEATHER.api_precipitation_sum} L/m²` },
     get temperatura_min_avui() { return `${WEATHER.api_temperature_2m_min} °C` },
     get temperatura_max_avui() { return `${WEATHER.api_temperature_2m_max} °C` },
     get humitat_relativa() { return `${WEATHER.api_relativehumidity_2m} %` },
@@ -46,7 +70,7 @@ const WEATHER_GET = {
 }
 
 const DATA_GET = {
-    get evapotranspiració() { return `${DATA.api_evapotranspiration} mm` },
+    get evapotranspiració() { return `${DATA.api_evapotranspiration} L/m²` },
     get punt_rosada() { return `${DATA.api_dewpoint_2m} °C` },
     get cota_neu() { return `${DATA.api_freezinglevel_height} m` },
     get humitat_terra_3cm() { return `${DATA.api_soil_moisture_1_3cm} %` },
@@ -59,16 +83,19 @@ const DATA_GET = {
 }
 
 apiFetch()
+createOptions()
 
 async function apiFetch() {
 
-    const RESPONSE = await fetch(API_URL[1])
+    console.log(`Fetching... ${API_URL[location_number][0].match(/[^\d][^\s]\S{1,}/gi)}`)
+
+    const RESPONSE = await fetch(`${API_URL[0][0]}${API_URL[location_number][1]}${API_URL[0][1]}${API_URL[location_number][2]}${API_URL[0][2]}`)
     const API_DATA = await RESPONSE.json()
 
     let current_time_index = API_DATA.hourly.time.indexOf(API_DATA.current_weather.time)
 
     WEATHER.api_current_time = API_DATA.current_weather.time
-    INFO.date_hora = `${DATE.getHours()}:${getMinutesFormat()} h`
+    INFO.date_hora = `${getHoursFormat()}:${getMinutesFormat()} h`
     INFO.api_current_date = API_DATA.current_weather.time
     INFO.api_latitude = API_DATA.latitude
     INFO.api_longitude = API_DATA.longitude
@@ -125,9 +152,14 @@ async function apiFetch() {
     showTables()
     weekForecast()
     isDay()
+    writeTitle()
+    dateFormat()
 }
 
 function createTables(i, table) {
+
+
+
 
     let all_object_properties = [...Object.keys(WEATHER_GET), ...Object.keys(INFO_GET), ...Object.keys(DATA_GET)]
     let all_object_values = [...Object.values(WEATHER_GET), ...Object.values(INFO_GET), ...Object.values(DATA_GET)]
@@ -167,7 +199,7 @@ function showTables() {
     dateText(3)
 
     document.getElementById('nit_passada').innerText = `Nit de dia ${dateText(2)} (de 00:00h a 08:00h)`
-    document.getElementById('val-1').innerText = `${Math.min(PREVIOUS_NIGHT.api_rain[48], PREVIOUS_NIGHT.api_rain[49], PREVIOUS_NIGHT.api_rain[50], PREVIOUS_NIGHT.api_rain[51], PREVIOUS_NIGHT.api_rain[52], PREVIOUS_NIGHT.api_rain[53], PREVIOUS_NIGHT.api_rain[54], PREVIOUS_NIGHT.api_rain[55], PREVIOUS_NIGHT.api_rain[56])} L`
+    document.getElementById('val-1').innerText = `${Math.min(PREVIOUS_NIGHT.api_rain[48], PREVIOUS_NIGHT.api_rain[49], PREVIOUS_NIGHT.api_rain[50], PREVIOUS_NIGHT.api_rain[51], PREVIOUS_NIGHT.api_rain[52], PREVIOUS_NIGHT.api_rain[53], PREVIOUS_NIGHT.api_rain[54], PREVIOUS_NIGHT.api_rain[55], PREVIOUS_NIGHT.api_rain[56])} L/m²`
     document.getElementById('val-2').innerText = `${Math.min(PREVIOUS_NIGHT.api_temperature_2m[48], PREVIOUS_NIGHT.api_temperature_2m[49], PREVIOUS_NIGHT.api_temperature_2m[50], PREVIOUS_NIGHT.api_temperature_2m[51], PREVIOUS_NIGHT.api_temperature_2m[52], PREVIOUS_NIGHT.api_temperature_2m[53], PREVIOUS_NIGHT.api_temperature_2m[54], PREVIOUS_NIGHT.api_temperature_2m[55], PREVIOUS_NIGHT.api_temperature_2m[56])} °C`
     document.getElementById('val-3').innerText = `${Math.min(PREVIOUS_NIGHT.api_apparent_temperature[48], PREVIOUS_NIGHT.api_apparent_temperature[49], PREVIOUS_NIGHT.api_apparent_temperature[50], PREVIOUS_NIGHT.api_apparent_temperature[51], PREVIOUS_NIGHT.api_apparent_temperature[52], PREVIOUS_NIGHT.api_apparent_temperature[53], PREVIOUS_NIGHT.api_apparent_temperature[54], PREVIOUS_NIGHT.api_apparent_temperature[55], PREVIOUS_NIGHT.api_apparent_temperature[56])} °C`
     document.getElementById('val-4').innerText = `${Math.min(PREVIOUS_NIGHT.api_windgusts_10m[48], PREVIOUS_NIGHT.api_windgusts_10m[49], PREVIOUS_NIGHT.api_windgusts_10m[50], PREVIOUS_NIGHT.api_windgusts_10m[51], PREVIOUS_NIGHT.api_windgusts_10m[52], PREVIOUS_NIGHT.api_windgusts_10m[53], PREVIOUS_NIGHT.api_windgusts_10m[54], PREVIOUS_NIGHT.api_windgusts_10m[55], PREVIOUS_NIGHT.api_windgusts_10m[56])} Km/h`
@@ -178,7 +210,7 @@ function showTables() {
     document.getElementById('prediccio_dema').innerText = `Predicció de demà dia ${dateText(3)} basada en l'informació més recent.`
     document.getElementById('val-7').innerText = `${NEXT_DAY.api_temperature_2m_min} °C`
     document.getElementById('val-8').innerText = `${NEXT_DAY.api_temperature_2m_max} °C`
-    document.getElementById('val-9').innerText = `${Math.round(NEXT_DAY.api_precipitation_sum * 0.7)} - ${Math.round(NEXT_DAY.api_precipitation_sum)} L`
+    document.getElementById('val-9').innerText = `${Math.round(NEXT_DAY.api_precipitation_sum * 0.7)} - ${Math.round(NEXT_DAY.api_precipitation_sum)} L/m²`
     document.getElementById('val-10').innerText = `${NEXT_DAY.api_windspeed} Km/h`
     document.getElementById('val-11').innerText = `${windCardinalDirection(NEXT_DAY.api_winddirection)} `
 
@@ -201,6 +233,9 @@ function weatherCode(code) {
         case 63: return 'Pluja'
         case 65: return 'Pluja intensa'
         case 66: case 67: return 'Pluja gelada'
+        case 71: return 'Neu lleugera'
+        case 73: return 'Neu'
+        case 75: return 'Molta neu'
         case 77: return 'Gebre'
         case 80: return 'Ruixat lleuger'
         case 81: return 'Ruixat moderat'
@@ -236,13 +271,13 @@ function windCardinalDirection(degrees) {
     // ↖ ↗ ↘ ↙ ← → ↑ ↓
     switch (closest_degree) {
         case 0: return `N ↑ (${degrees}°)`
-        case 22.5: return `NNE ↑↗ (${degrees}°)`
+        case 22.5: return `NNE ↑ (${degrees}°)`
         case 45: return `NE ↗ (${degrees}°)`
         case 67.5: return `NEE → (${degrees}°)`
         case 90: return `E → (${degrees}°)`
         case 112.5: return `SEE → (${degrees}°)`
         case 135: return `SE ↘ (${degrees}°)`
-        case 157.5: return `SSE ↓↘ (${degrees}°)`
+        case 157.5: return `SSE ↓ (${degrees}°)`
         case 180: return `S ↓ (${degrees}°)`
         case 202.5: return `SSO ↓ (${degrees}°)`
         case 225: return `SO ↙ (${degrees}°)`
@@ -250,7 +285,7 @@ function windCardinalDirection(degrees) {
         case 270: return `O ← (${degrees}°)`
         case 292.5: return `NOO ← (${degrees}°)`
         case 315: return `NO ↖ (${degrees}°)`
-        case 337.5: return `NNO ↑ (${degrees}°)`
+        case 337.5: return `NNO ↖ (${degrees}°)`
     }
 }
 
@@ -280,8 +315,8 @@ function weatherIcon(weathercode) {
 
     switch (weathercode) {
         case 0:
-            if (isDay()) { FORECAST.color = '#ffdc25'; return 'sun' }
-            if (!isDay()) { FORECAST.color = '#091725'; return 'night' }
+            if (isDay() == true) { FORECAST.color = '#ffdc25'; return 'sun' }
+            if (isDay() == false) { FORECAST.color = '#091725'; return 'night' }
         case 1: case 2: FORECAST.color = '#8ab2d1'; return 'part_cloud'
         case 3: FORECAST.color = '#bcbcbc'; return 'cloud'
         case 45: case 48: FORECAST.color = '#bcbcbc'; return 'fog'
@@ -299,12 +334,97 @@ function isDay() {
     let sortida_min = Number(INFO_GET.sortida_sol.match(/[^\w](\d\d)/)[1]) + (Number(INFO_GET.sortida_sol.match(/\d\d/)) * 60)
     let posta_min = Number(INFO_GET.posta_sol.match(/[^\w](\d\d)/)[1]) + (Number(INFO_GET.posta_sol.match(/\d\d/)) * 60)
     let min = (INFO.hores * 60) + INFO.minuts
-    if (min >= sortida_min || min <= posta_min) { return true }
-    else { return false }
+    if (min >= sortida_min && min <= posta_min) { return true }
+    if (min < sortida_min || min > posta_min) { return false }
 }
 
+function getHoursFormat() {
+    if (DATE.getHours() < 10) { return '0' + DATE.getHours() }
+    else { return DATE.getHours() }
+}
 function getMinutesFormat() {
     if (DATE.getMinutes() < 10) { return '0' + DATE.getMinutes() }
     else { return DATE.getMinutes() }
 }
 
+function dateFormat() {
+    let unformatted = `${INFO.api_current_date.match(/^........../)}`
+    let date_array = []
+    date_array[0] = unformatted.match(/(\d\d)$/)
+    date_array[1] = unformatted.match(/(\d\d)\S\d\d$/)
+    date_array[2] = unformatted.match(/^(\d\d\d\d)/)
+    return `${date_array[0][1]}-${monthCharacter(date_array[1][1])}-${date_array[2][1]}`
+}
+
+function monthCharacter(month_number) {
+    switch (month_number) {
+        case '01': return 'GEN'
+        case '02': return 'FEB'
+        case '03': return 'MAR'
+        case '04': return 'ABR'
+        case '05': return 'MAI'
+        case '06': return 'JUN'
+        case '07': return 'JUL'
+        case '08': return 'AGO'
+        case '09': return 'SET'
+        case '10': return 'OCT'
+        case '11': return 'NOV'
+        case '12': return 'DES'
+    }
+}
+
+function wipe() {
+    const WIPE_CUE = [WEATHER, DATA, PREVIOUS_NIGHT, NEXT_DAY, FORECAST]
+
+    for (let i = 0; i < WIPE_CUE.length; i++) {
+        for (const key in WIPE_CUE[i]) {
+            delete WIPE_CUE[i][key];
+        }
+    }
+    document.getElementById('table-1').innerHTML = ''
+    document.getElementById('table-2').innerHTML = ''
+    document.getElementById('table-3').innerHTML = ''
+    document.getElementById('val-6').innerHTML = ''
+    document.getElementById('val-12').innerHTML = ''
+    for (let i = 1; i <= 7; i++) { document.getElementById('div_dia_' + i).className = 'dia' }
+    console.clear()
+}
+
+function change(new_location_number) {
+
+    wipe()
+    location_number = new_location_number
+    apiFetch()
+}
+
+function selectLocation() {
+
+    let select_value = document.getElementById("form-select")
+    console.log(select_value.value)
+    change(select_value.value)
+}
+
+function createOptions() {
+
+    let select_value = document.getElementById("form-select")
+
+    for (let i = 1; i < API_URL.length; i++) {
+
+        let new_option = document.createElement('option')
+        select_value.append(new_option)
+        new_option.setAttribute('value', i)
+        new_option.innerText = API_URL[i][0].match(/[^\d][^\s][a-z\sàèéíòóùúäëïöïü]{1,}/gi)
+    }
+}
+
+function navbarFormat(unformatted_name) {
+    let formatted_name = String(unformatted_name.match(/[^\d][^\s][a-z\sàèéíòóùúäëïöïü]{1,}/gi))
+    formatted_name = formatted_name.replace(/\s/ig, '').toLocaleLowerCase();
+    return formatted_name
+}
+
+function writeTitle() {
+    document.getElementById('navbar_title').innerText = `meteo${navbarFormat(API_URL[location_number][0])}`
+
+
+}
